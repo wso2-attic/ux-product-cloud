294354b831cde760b5d7a3c3242766a685ff364c
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2015-06-03 13:46:35 +0530

    add file upload and create button

--- Markup/js/custom/custom.js
+++ Markup/js/custom/custom.js
@@ -77,3 +77,25 @@ $('.cloud-menu-popover').popover({
 
     })
 
+
+/**
+ * Use to handle file upload
+ */
+$(document).on('change', '.btn-file :file', function() {
+    var input = $(this),
+        numFiles = input.get(0).files ? input.get(0).files.length : 1,
+        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+    input.trigger('fileselect', [numFiles, label]);
+});
+
+$('.btn-file :file').on('fileselect', function(event, numFiles, label) {
+
+        var input = $(this).parents('.input-group').find(':text'),
+            log = numFiles > 1 ? numFiles + ' files selected' : label;
+
+        if( input.length ) {
+            input.val(log);
+        } else {
+            if( log ) alert(log);
+        }
+});
