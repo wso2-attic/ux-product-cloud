b38442d0f86d9dc10d633eba4115251d36c0a21c
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2015-09-07 16:37:14 +0530

    fix popover issue

--- js/custom/custom.js
+++ js/custom/custom.js
@@ -135,3 +135,12 @@ $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
     };
 
 }( jQuery ));
+
+//fix popover close issue
+$('body').on('click', function (e) {
+    $('[data-toggle="popover"]').each(function () {
+        if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+            $(this).popover('hide');
+        }
+    });
+});
