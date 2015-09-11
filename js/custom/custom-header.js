fb9b5127cc33c6f82918de3c3ae34a3fb5bdca5b
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2015-09-11 16:14:05 +0530

    add popover fix to header

--- js/custom/custom-header.js
+++ js/custom/custom-header.js
@@ -7,3 +7,12 @@ $('.cloud-menu-popover').popover({
         return $("#popover-content").html();
     }
 });
+
+//fix popover close issue
+$('body').on('click', function (e) {
+    $('[data-toggle="popover"]').each(function () {
+        if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+            $(this).popover('hide');
+        }
+    });
+});
