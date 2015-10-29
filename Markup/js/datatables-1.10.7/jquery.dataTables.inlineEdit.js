36752dd25d7ed315f7371858214895dd38033952
Imesh Chandrasiri <imeshc@wso2.com>
2015-10-29 14:05:22 +0530

    Data tables inline edit plugin fix

--- Markup/js/datatables-1.10.7/jquery.dataTables.inlineEdit.js
+++ Markup/js/datatables-1.10.7/jquery.dataTables.inlineEdit.js
@@ -9,63 +9,45 @@
  * include  Jeditable inorder to use this plugin
  */
 
-(function ($) {
+(function($){
 
-    $.fn.inlineEdit = function (target, table, url, options) {
-        //get col of value editing
-      //  console.log('col_name: ' + table.cell(target).index().column)
-        //get current row id
-     //   console.log(table.cell(target).index().row)
-        //get current row first value
-     //   console.log(table.row(table.cell(target).index().row).data()['varname'])
-     //   console.log(table.row().data())
+    $.fn.inlineEdit = function(cell,table){
 
-        if(target.hasClass('inline-edit') && target.is('td')){
-
-            var inlineText = target.text();
-            //enable inline editing
-            var me = target.editable(
-                function(value, settings) {
-
-                    //inline validation
-                   /* if( !value.match(/^\d{3}\-\d{4}$/) ){
-                        console.log(value + " is not valid zip-code");
-                        table.cell(this).data(inlineText)
-                    }else{*/
-                        if(value){
-                            inlineText = value;
-
-                            if(inlineText){
-                                table.cell(this).data(inlineText)
-                            }
-                        }
-                  /* }*/
-                    //var editedData = table.row(target.parents()).data();
-                    var editedData = table.cell(target).data();
-                    var cellId = table.row(target.parents()).data().id;
-                    var developmentCell = table.row(target.parents()).data().dev;
-                    var testingCell = table.row(target.parents()).data().test;
-                    //var deployCell = table.row(target.parents()).data().deploy;
-                    var edits = {};
-                    edits['id'] = cellId;
-                    if(target.hasClass('development')){
-                        edits['development'] = developmentCell;
-                    }else{
-                        edits['testing'] = testingCell;
-                    }
-                    console.log(edits);
-                    //console.log(cellId);
-                    //console.log(value);
-                    //console.log(settings);
-                },{
-                    submit    : 'OK',
-                    indicator : '<i class="fw fw-wso2-logo fw-pulse fw-2x"></i>',
-                    tooltip   : 'Click to edit...'
-                }
-            );
-
-
+        if(typeof $.fn.editable === 'undefined'){
+            throw 'Dependancy not found : Jeditable';
         }
 
+        var target  = $(cell),
+            jTable   = table;
+
+        if(target.hasClass('inline-edit') && target.is('td')){
+            var edit = target.editable(function(value,settings){
+                if(value){
+                    inlineText = value;
+
+                    if(inlineText){
+                        jTable.cell(this).data(inlineText)
+                    }
+
+                    var editedCell  = table.cell(target),
+                        columnIndex = editedCell[0][0].column,
+                        columnText  = table.column(columnIndex).header().innerText.trim(),
+                        cellId      = table.row(target.parents()).data().id,
+                        editContent = target.text(),
+                        editedData  = {};
+
+                    editedData['id'] = cellId;
+                    editedData[columnText] = editContent;
+
+                    console.log(editedData);
+                }
+            },{
+                submit      : 'OK',
+                indicator   : '<i class="fw fw-wso2-logo fw-pulse fw-2x"></i>',
+                tooltip     : 'Click to edit...'
+            })
+        }
     }
-})(jQuery);
+
+})(jQuery)
+
